5c9259796cf7d9e3db89a79d810c22b8032aab44
antoninbeaufort <antonin.beaufort@gmail.com>
2022-08-16 11:16:43 +0200

    ✅ add unit test for validateCardExpiration

--- app/utils.test.ts
+++ app/utils.test.ts
@@ -1,4 +1,4 @@
-import { validateEmail } from "./utils";
+import { validateEmail, validateCardExpiration } from "./utils";
 
 test("validateEmail returns false for non-emails", () => {
   expect(validateEmail(undefined)).toBe(false);
@@ -11,3 +11,19 @@ test("validateEmail returns false for non-emails", () => {
 test("validateEmail returns true for emails", () => {
   expect(validateEmail("kody@example.com")).toBe(true);
 });
+
+test("validateCardExpiration return false for non-valid card expiration", () => {
+  expect(validateCardExpiration(undefined)).toBe(false);
+  expect(validateCardExpiration(null)).toBe(false);
+  expect(validateCardExpiration(false)).toBe(false);
+  expect(validateCardExpiration(true)).toBe(false);
+  expect(validateCardExpiration({})).toBe(false);
+  expect(validateCardExpiration([])).toBe(false);
+  expect(validateCardExpiration("05/22")).toBe(false);
+  expect(validateCardExpiration("01/21")).toBe(false);
+})
+
+test("validateCardExpiration return true for valid card expiration", () => {
+  expect(validateCardExpiration("05/24")).toBe(true);
+  expect(validateCardExpiration("12/22")).toBe(true);
+})
